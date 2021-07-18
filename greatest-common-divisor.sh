96b9ec63f86541cf30555a48497ceda449d50667
watsuyo <dmwtpdmwtp24@gmail.com>
2021-07-18 23:41:23 +0900

    create greatest-common-divisor.sh

---
+++ greatest-common-divisor.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+regex=^[0-9]+$
+
+first_arg=$1
+second_arg=$2
+
+if [[ !("$first_arg" =~ $regex && "$second_arg" =~ $regex) ]]; then
+	echo "引数に整数を2つ入力してください。"
+	exit $?
+fi
+
+result=null
+
+while [ $result != 0 ]
+do
+	result=$(($first_arg % $second_arg))
+	first_arg=$second_arg
+	second_arg=$result
+done
+
+echo "最大公約数: $first_arg"
